ae3aa1954ebe12a4b39a0038b1961a6c86ce29bf
JEONGKUNHO <dghj6739@gmail.com>
2025-04-02 19:49:31 +0900

    Initial Commit

---
+++ CafeMap/CafeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeMapApp.swift
+//  CafeMap
+//
+//  Created by 정건호 on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CafeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CafeMapTests/CafeMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  CafeMapTests.swift
+//  CafeMapTests
+//
+//  Created by 정건호 on 4/2/25.
+//
+
+import Testing
+@testable import CafeMap
+
+struct CafeMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
